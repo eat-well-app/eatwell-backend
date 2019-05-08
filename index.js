f0a693184abda95d8b597c09fab2536a6e27d942
Casey Bee <caseye.baker@gmail.com>
2019-05-07 20:33:41 -0700

    :tada: initial commit, server starter

---
+++ index.js
@@ -0,0 +1,16 @@
+//== Dependencies ==//
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const cors = require("cors");
+
+const server = express();
+
+const port = process.env.PORT || 3300;
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.json({message: 'Server up!'})
+})
