cc1e0ced4a0e5de8273ca44242c21ecbc4d65835
Casey Bee <caseye.baker@gmail.com>
2019-05-09 23:18:45 -0700

    comments out server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-//== Dependencies ==//
-const express = require("express");
-const helmet = require("helmet");
-const knex = require("knex");
-const cors = require("cors");
+// //== Dependencies ==//
+// const express = require("express");
+// const helmet = require("helmet");
+// const knex = require("knex");
+// const cors = require("cors");
 
-const server = express();
+// const server = express();
 
-server.use(helmet());
-server.use(express.json());
+// server.use(helmet());
+// server.use(express.json());
 
-server.get("/", (req, res) => {
-    res.json({message: 'Server up!'})
-})
+// server.get("/", (req, res) => {
+//     res.json({message: 'Server up!'})
+// })
 
-server.get("/routes", (req, res)=> {
-    res.json({})
-})
+// server.get("/routes", (req, res)=> {
+//     res.json({})
+// })
 
-module.exports = server
+// module.exports = server
